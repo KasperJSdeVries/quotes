abcf697d84d6e4766895e26621876278a60777b8
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-10-08 21:42:59 +0200

    Initial Commit

---
+++ src/handlers.rs
@@ -0,0 +1,21 @@
+use axum::{extract, http};
+use serde::Deserialize;
+use sqlx::PgPool;
+
+#[derive(Debug, Deserialize)]
+pub struct CreateQuote {
+    book: String,
+    quote: String,
+}
+
+pub async fn health() -> http::StatusCode {
+    http::StatusCode::OK
+}
+
+pub async fn create_quote(
+    extract::State(pool): extract::State<PgPool>,
+    axum::Json(payload): axum::Json<CreateQuote>,
+) -> http::StatusCode {
+    println!("{:?}", payload);
+    http::StatusCode::CREATED
+}
